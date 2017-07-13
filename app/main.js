ada2d709e615599d290b25ff67e7a9ea9501ca3e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-07-13 14:44:13 +0200

    add: eslint

--- app/main.js
+++ app/main.js
@@ -3,10 +3,10 @@ import './style.scss';
 import './jlg-layout/jlg-layout.js';
 
 const app = angular.module('main', [
-    'jlg-layout',
+	'jlg-layout',
 ]);
 
 app.run(($rootScope) => {
-    'ngInject';
-    $rootScope.name = 'Jean-Louis';
+	'ngInject';
+	$rootScope.name = 'Jean-Louis';
 });
