6ededa46cccdb7c5e738efb2571c26d8b6a95cec
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-07-13 14:55:36 +0200

    add: jlg-layout

--- app/jlg-layout/jlg-layout.js
+++ app/jlg-layout/jlg-layout.js
@@ -6,3 +6,17 @@ import jlgHeaderUrl from './tmpl/jlg-header.html';
 app.component('jlgHeader', {
 	template: jlgHeaderUrl
 });
+
+import jlgBodyUrl from './tmpl/jlg-body.html';
+app.component('jlgBody', {
+	template: jlgBodyUrl,
+	controller: function() {
+		'ngInject';
+		this.name = 'Jean-Louis';
+	}
+});
+
+import jlgFooterUrl from './tmpl/jlg-footer.html';
+app.component('jlgFooter', {
+	template: jlgFooterUrl
+});
--- app/main.js
+++ app/main.js
@@ -2,11 +2,7 @@ import './style.scss';
 
 import './jlg-layout/jlg-layout.js';
 
-const app = angular.module('main', [
+angular.module('main', [
 	'jlg-layout',
 ]);
 
-app.run(($rootScope) => {
-	'ngInject';
-	$rootScope.name = 'Jean-Louis';
-});
